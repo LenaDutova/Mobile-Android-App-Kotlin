a99b26e32ac88505060d77f54d2df84fc1ef80d3
DEA <lena.dutova@list.ru>
2025-09-28 22:59:51 +0300

    add back listener to return

--- app/src/main/java/com/mobile/vedroid/kt/fragments/ReturningFragment.kt
+++ app/src/main/java/com/mobile/vedroid/kt/fragments/ReturningFragment.kt
@@ -3,6 +3,7 @@ package com.mobile.vedroid.kt.fragments
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import androidx.activity.addCallback
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.button.MaterialButtonToggleGroup
@@ -18,27 +19,29 @@ class ReturningFragment : Fragment(R.layout.fragment_returning) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        activity?.debugging("HI")
-
-//        activity?.onBackPressedDispatcher.addCallback() {
-//            findNavController().navigate(R.id.action_screen_register_return_start)
-//        }
+        debugging("HI")
 
         login = view.findViewById(R.id.login)
         toggle = view.findViewById(R.id.toggle_button)
 
+        requireActivity().onBackPressedDispatcher.addCallback(this) {
+            debugging("Back stack click")
+            findNavController().navigateUp()
+        }
+
         val btnStart : Button = view.findViewById(R.id.btn_to_start)
         btnStart.setOnClickListener {
-            debugging("Click to final")
-
             if (!login.text.isNullOrBlank() && toggle.checkedButtonId != R.id.btn_not_defined) {
+                debugging("Click from returning with registration data")
+
                 val args = Bundle()
                 args.putString(SingleActivity.LOGIN, login.text.toString())
                 args.putBoolean(SingleActivity.GENDER, (toggle.checkedButtonId == R.id.btn_man))
 
                 findNavController().navigate(R.id.action_screen_register_return_start, args)
-                return@setOnClickListener
             } else {
+                debugging("Click from returning, but without some params")
+
                 if (login.text.isNullOrBlank()) (activity as SingleActivity).showSnackBar("Enter name")
                 if (toggle.checkedButtonId == R.id.btn_not_defined) (activity as SingleActivity).showToast("Choose gender")
             }
--- app/src/main/java/com/mobile/vedroid/kt/fragments/StartFragment.kt
+++ app/src/main/java/com/mobile/vedroid/kt/fragments/StartFragment.kt
@@ -12,29 +12,37 @@ import com.mobile.vedroid.kt.extensions.debugging
 
 class StartFragment : Fragment(R.layout.fragment_start) {
 
+    lateinit var greeting : TextView
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         debugging("HI")
 
+        greeting = view.findViewById(R.id.tv_greeting)
+
         val btnFinal : Button = view.findViewById(R.id.btn_to_final)
         btnFinal.setOnClickListener {
-            activity?.debugging("Click to final")
+            debugging("Click to final")
             findNavController().navigate(R.id.action_screen_start_to_final)
         }
 
         val btnReturning : Button = view.findViewById(R.id.btn_to_returning)
         btnReturning.setOnClickListener {
-            activity?.debugging("Click to returning")
+            debugging("Click to returning")
             findNavController().navigate(R.id.action_screen_start_to_register)
         }
 
         if (arguments != null && !arguments?.getString(SingleActivity.Companion.LOGIN).isNullOrEmpty()){
-            var txt : String? = "Welcome, "
-            txt += if (arguments?.getBoolean(SingleActivity.Companion.GENDER) ?: false) "Mr. " else "Mrs. "
-            txt += arguments?.getString(SingleActivity.Companion.LOGIN) + "!"
-
-            val greeting : TextView = view.findViewById(R.id.tv_greeting)
-            greeting.text = txt
+            showGreeting(requireArguments())
         }
+    }
+
+    private fun showGreeting(bundle: Bundle){
+        var txt : String? = getString(R.string.text_greeting) + " "
+        txt += if (bundle.getBoolean(SingleActivity.Companion.GENDER, false)) getString(R.string.text_mr) else getString(R.string.text_mrs)
+        txt += " " + bundle.getString(SingleActivity.Companion.LOGIN) + "!"
+        debugging("Greeting: $txt")
+
+        greeting.text = txt
     }
 }
